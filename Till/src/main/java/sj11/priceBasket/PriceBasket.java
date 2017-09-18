9608d0ea9db77dbee5d21e4983a2891e34817cda
SeRGiO11 <sergio.anger@gmail.com>
2017-09-18 23:50:36 +0200

    Added a plugin to be able to execute from command line. Renaming to meet requirements.

---
+++ Till/src/main/java/sj11/priceBasket/PriceBasket.java
@@ -0,0 +1,11 @@
+package sj11.priceBasket;
+
+public class PriceBasket {
+
+    public static void main(String[] args) {
+        for (String item : args) {
+            System.out.println("Item: " + item);
+        }
+    }
+
+}
