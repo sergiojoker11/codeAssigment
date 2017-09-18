38ffa6c18c16eabc70e7d578f6b50c9ba897baf3
Sergio Martin Sanchez <sergio.martinsanchez@costain.com>
2017-09-18 18:36:12 +0100

    Created Maven(Java) project

---
+++ Till/src/main/java/sj11/till/Till.java
@@ -0,0 +1,12 @@
+package sj11.till;
+
+public class Till {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Here I start");
+    }
+
+}
