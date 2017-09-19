e3edacc76591751741bd4bbeb4ad2c0e26d93cf1
SeRGiO11 <sergio.anger@gmail.com>
2017-09-20 00:27:47 +0200

    Removed some comments Set to false the Hibernate logging for queries. (This was supposed to be done in the last comment)

--- Till/src/main/java/sj11/priceBasket/config/PersistenceConfiguration.java
+++ Till/src/main/java/sj11/priceBasket/config/PersistenceConfiguration.java
@@ -1,20 +1,3 @@
-/*
- * THIS SOFTWARE IS PROVIDED BY COSTAIN LTD ``AS IS'', WITH NO WARRANTY, TERM
- * OR CONDITION OF ANY KIND, EXPRESS OR IMPLIED, AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
- * EVENT SHALL COSTAIN LTD BE LIABLE FOR ANY LOSSES, CLAIMS OR DAMAGES OF
- * WHATEVER NATURE, INCLUDING ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGES OR LOSSES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE.
- *
- * Copyright 2017 © Costain Ltd.
- * All Rights Reserved.
- */
 package sj11.priceBasket.config;
 
 import javax.persistence.EntityManagerFactory;
@@ -64,7 +47,7 @@ public class PersistenceConfiguration {
         // will set the provider to 'org.hibernate.ejb.HibernatePersistence'
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         // will set hibernate.show_sql to 'true'
-        vendorAdapter.setShowSql(true);
+        vendorAdapter.setShowSql(false);
         // if set to true, will set hibernate.hbm2ddl.auto to 'update'
         vendorAdapter.setGenerateDdl(false);
 
